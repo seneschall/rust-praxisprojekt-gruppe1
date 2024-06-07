04be236d0cca909de280fe6f219eeb4b14862152
MartinPerezLorenzo <martin.p-l@filesteak.com>
2024-06-07 12:26:38 +0200

    added outgoing edges compare test with digraph and wtdigraph

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,7 +1,7 @@
 use wt_graphs::graph::{create_sequence_and_bitmap, import_adjacency_list, import_graph_properties, wt_graph::WTDigraph, Digraph};
 
 pub fn setup_digraph(filename : &str) -> Digraph<u32,u32>{
-    let (e_count,v_count) = import_graph_properties(filename);
+    let (v_count,e_count) = import_graph_properties(filename);
     let adj: Vec<Vec<u32>> = import_adjacency_list(filename); 
     let digraph : Digraph<u32,u32> = Digraph::new2(v_count, e_count,adj ); // temporary new2 to create Digraph with adj list
     digraph
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -12,15 +12,15 @@ mod test {
     #[test]
     fn test_graph() {
         let mut digraph = setup_digraph("tests/tinyDG.txt");
-        assert_eq!(digraph.e_count(), 13);
-        assert_eq!(digraph.v_count(), 22);
+        assert_eq!(digraph.v_count(), 13);
+        assert_eq!(digraph.e_count(), 22);
         assert_eq!(digraph.outgoing_edges(2), vec![3u32, 0u32]);
         assert_eq!(digraph.outgoing_edges(1), Vec::new());
         digraph.add_edge(1, 0);
-        assert_eq!(digraph.e_count(), 14);
+        assert_eq!(digraph.e_count(), 23);
         assert_eq!(digraph.outgoing_edges(1), vec![0u32]);
         digraph.delete_edge(1,0);
-        assert_eq!(digraph.e_count(),13);
+        assert_eq!(digraph.e_count(),22);
         assert_eq!(digraph.outgoing_edges(1), Vec::new());
     }
     #[test]
@@ -40,5 +40,16 @@ mod test {
         assert_eq!(wtdigraph.outgoing_edges(1), Vec::new());
         assert_eq!(wtdigraph.e_count(), 22);
         assert_eq!(wtdigraph.v_count(), 13);
+    }
+    #[test]
+    fn test_compare_outgoing_edges_wtdigraph_with_digraph(){
+        let digraph = setup_digraph("tests/tinyDG.txt");
+        let wtdigraph = setup_wtdigraph("tests/tinyDG.txt");
+        assert_eq!(digraph.v_count(), 13);
+        for i in 0..digraph.v_count(){
+            assert_eq!(digraph.outgoing_edges(i), wtdigraph.outgoing_edges(i));
+        }
+        assert_eq!(digraph.v_count(),wtdigraph.v_count());
+        assert_eq!(digraph.e_count(),wtdigraph.e_count());
     }
 }
