e0c13930094c4bdefc5f566c4a107d62349b0a9f
MartinPerezLorenzo <martin.p-l@filesteak.com>
2024-06-16 17:12:06 +0200

    fixed traits.rs, removed generics

--- src/traits.rs
+++ src/traits.rs
@@ -3,96 +3,82 @@ use num::{ToPrimitive, Unsigned};
 
 
 // this trait applies to all graph structures
-pub trait Graph<T, L>
-where
-    T: Unsigned + ToPrimitive,
+pub trait Graph<L>
 {
-    fn add_edge(&mut self, v: T, w: T);
+    fn add_edge(&mut self, v: usize, w: usize);
 
-    fn add_vertex(&mut self, v: T); // adds vertex at given index; use at users own risk; if vertex exists (i.e. vertex is less than wt_adj.len()), it just adds it, if it does, it must not have incoming or outgoing edges
+    fn add_vertex(&mut self, v: usize); // adds vertex at given index; use at users own risk; if vertex exists (i.e. vertex is less than wt_adj.len()), it just adds it, if it does, it must not have incoming or outgoing edges
 
-    fn add_vertex_label(&mut self, v: T, label: L);
+    fn add_vertex_label(&mut self, v: usize, label: L);
 
-    fn append_vertex(&mut self, v: T) -> T; // adds vertex at position wt_adj.len() or at index of lowest deleted vertex (if that change hasn't been committed)
+    fn append_vertex(&mut self, v: usize) -> usize; // adds vertex at position wt_adj.len() or at index of lowest deleted vertex (if that change hasn't been committed)
 
-    fn delete_edge(&mut self, v: T, w: T);
+    fn delete_edge(&mut self, v: usize, w: usize);
 
-    fn delete_vertex(&mut self, v: T); // should eventually be changed to return a Result type
+    fn delete_vertex(&mut self, v: usize); // should eventually be changed to return a Result type
 
-    fn e_count(&self) -> T; // should eventually be changed to return a Result type
+    fn e_count(&self) -> usize; // should eventually be changed to return a Result type
 
-    fn edit_label(&mut self, v: T, change: L); // true if last item in uncommitted edits for v is Edit::DeleteSelf
+    fn edit_label(&mut self, v: usize, label: L); // true if last item in uncommitted edits for v is Edit::DeleteSelf
 
-    fn get_label(&self, v: T) -> Option<&L>;
+    fn get_label(&self, v: usize) -> Option<&L>;
 
-    fn v_count(&self) -> T;
+    fn v_count(&self) -> usize;
 
-    fn vertex_deleted(&self, v: T) -> bool;
+    fn vertex_deleted(&self, v: usize) -> bool;
 }
 
 
 // this trait applies to undirected graph structures
-pub trait Undirected<T>
-where
-    T: Unsigned + ToPrimitive,
+pub trait Undirected
 {
-    fn edges(&self, vertex: T) -> Vec<T>;
+    fn edges(&self, vertex: usize) -> Vec<usize>;
 }
 
 
 // this trait applies to directed graph structures
-pub trait Directed<T>
-where
-    T: Unsigned + ToPrimitive,
+pub trait Directed
 {
-    fn outgoing_edges(&self, vertex: T) -> Vec<T>; // should probably be changed to return an iterator instead
+    fn outgoing_edges(&self, vertex: usize) -> Vec<usize>; // should probably be changed to return an iterator instead
 
-    fn incoming_edges(&self, vertex: T) -> Vec<T>; // likewise here
+    fn incoming_edges(&self, vertex: usize) -> Vec<usize>; // likewise here
 }
 
 
 // this trait applies to weighted graph structures
-pub trait Weighted<T>
-where
-    T: Unsigned + ToPrimitive,
+pub trait Weighted
 {
-    fn weight_of_edge(&self, from: T, to: T) -> f64;
+    fn weight_of_edge(&self, from: usize, to: usize) -> f64;
 }
 
 
 
 //  this trait applies to all WT graph structures
-pub trait WT<T>
-where
-    T: Unsigned + ToPrimitive,
+pub trait WT<L>
 {
     fn commit_edits(&self);
 
-    fn get_uncommitted_edits(&self) -> Option<HashMap<T, L>>;
+    fn get_uncommitted_edits(&self) -> Option<HashMap<usize, L>>;
 
     fn discard_edits(&self);
 
-    fn shrink(&mut self) -> HashMap<T, T>; // removes all unconnected vertices from bitmap; only allowed, if has_uncommitted_edits == false; returns a Hashmap with old indices as keys and new indices as values
+    fn shrink(&mut self) -> HashMap<usize, usize>; // removes all unconnected vertices from bitmap; only allowed, if has_uncommitted_edits == false; returns a Hashmap with old indices as keys and new indices as values
 }
 
 
 // this trait applies to undirected WT graph structures
-pub trait WTGraph<T>
-where
-    T: Unsigned + ToPrimitive,
+pub trait WTGraph
 {
-    fn updated_edges(&self, v: T) -> Option<Vec<T>>;
+    fn updated_edges(&self, v: usize) -> Option<Vec<usize>>;
 }
 
 
 // this trait applies to directed WT structures
-pub trait WTDigraph<T>
-where
-    T: Unsigned + ToPrimitive,
+pub trait WTDigraph
 {
-    fn updated_outgoing_edges(&self, v: T) -> Option<Vec<T>>;
+    fn updated_outgoing_edges(&self, v: usize) -> Option<Vec<usize>>;
 
-    fn updated_incoming_edges(&self, v: T) -> Option<Vec<T>>;
+    fn updated_incoming_edges(&self, v: usize) -> Option<Vec<usize>>;
 }
 
 
@@ -108,15 +94,13 @@ pub enum ShortestPathAlgorithm {
     AStar,
 }
 
-pub trait GraphSearch<T>
-where
-    T: Unsigned + ToPrimitive,
+pub trait GraphSearch
 {
-    fn shortest_path(&self, from: T, to: T, mode: ShortestPathAlgorithm) -> Vec<T>; // returns the shortest path from `from` to `to` using breadth first search
+    fn shortest_path(&self, from: usize, to: usize, mode: ShortestPathAlgorithm) -> Vec<usize>; // returns the shortest path from `from` to `to` using breadth first search
 
-    fn shortest_paths(&self, mode: ShortestPathAlgorithm) -> Vec<Vec<T>>; // shortest paths from all vertices to all other vertices
+    fn shortest_paths(&self, mode: ShortestPathAlgorithm) -> Vec<Vec<usize>>; // shortest paths from all vertices to all other vertices
 
-    fn connected_components(&self) -> Vec<Vec<T>>; // returns all groups of vertices that are connected; makes no sense for directed graphs; default: bfs
+    fn connected_components(&self) -> Vec<Vec<usize>>; // returns all groups of vertices that are connected; makes no sense for directed graphs; default: bfs
 
-    fn connected(&self, a: T, b: T) -> bool; // is a connected to b? default: bfs
+    fn connected(&self, from: usize, to: usize) -> bool; // is a connected to b? default: bfs
 }
