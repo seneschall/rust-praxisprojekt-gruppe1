3c063aaca48cdf163eea4a9c4bdedc70b0a43589
Simon Sack <simon.c.sack@gmail.com>
2024-06-18 18:28:55 +0200

    changed delete traits and added labelled traits

--- src/traits.rs
+++ src/traits.rs
@@ -5,15 +5,15 @@ use std::collections::HashMap;
 pub trait Graph<L> {
     fn add_edge(&mut self, from: usize, to: usize);
 
+    fn add_ledge(&mut self, from: L, to: L); // get indices of `from` and `to` and call add_edge on those
+
     fn add_vertex(&mut self, vertex: usize); // adds vertex at given index; use at users own risk; if vertex exists (i.e. vertex is less than wt_adj.len()), it just adds it, if it does, it must not have incoming or outgoing edges
 
-    fn add_vertex_label(&mut self, vertex: usize, label: L);
+    fn add_lvertex(&mut self, label: L); // shortcut for `append_vertex` and `add_vertex_label`
+
+    fn add_vertex_label(&mut self, vertex: usize, label: L); // adds a label for the vertex with index `vertex`; every label must be unique! Method should check this and return a Result type
 
     fn append_vertex(&mut self, vertex: usize) -> usize; // adds vertex at position wt_adj.len() or at index of lowest deleted vertex (if that change hasn't been committed)
-
-    fn delete_edge(&mut self, from: usize, to: usize);
-
-    fn delete_vertex(&mut self, vertex: usize); // should eventually be changed to return a Result type
 
     fn e_count(&self) -> usize; // should eventually be changed to return a Result type
 
@@ -21,21 +21,53 @@ pub trait Graph<L> {
 
     fn get_label(&self, vertex: usize) -> Option<&L>;
 
-    fn v_count(&self) -> usize;
+    fn get_index(&self, label: L) -> Option<usize>; // returns the index of the vertex with the given label
+
+    fn v_count(&self) -> usize; // returns the number of vertices in graph
+}
+
+pub trait Delete<L> {
+    fn delete_edge(&mut self, from: usize, to: usize);
+
+    fn delete_and_shift(&mut self, vertex: usize); // deletes vertex at index and shifts all following indices 1 to the left
+
+    // fn delete_lvertex_and_shift(&mut self, label: L); // deletes vertex at index and shifts all following indices 1 to the left
+}
+
+pub trait WTDelete<L> {
+    fn delete_edge(&mut self, from: usize, to: usize);
+
+    fn delete_ledge(&mut self, from: L, to: L); // get indices of `from` and `to` and call delete_edge on those
+
+    fn delete_vertex(&mut self, vertex: usize); // should eventually be changed to return a Result type
+
+    // fn delete_lvertex(&mut self, label: L);
 
     fn vertex_deleted(&self, vertex: usize) -> bool;
 }
 
 // this trait applies to undirected graph structures
-pub trait Undirected {
-    fn edges(&self, vertex: usize) -> Vec<usize>;
+pub trait Undirected<L> {
+    fn edges(&self, vertex: usize) -> Vec<usize>; // returns all edges connected to vertex
+
+    // fn edges_lvertex(label: L) -> Vec<L>; // returns the labels of all edges connected to vertex with label
+
+    fn delete_edges(&self, vertex: usize); // deletes all edges connected to vertex
 }
 
 // this trait applies to directed graph structures
-pub trait Directed {
+pub trait Directed<L> {
     fn outgoing_edges(&self, vertex: usize) -> Vec<usize>; // should probably be changed to return an iterator instead
 
     fn incoming_edges(&self, vertex: usize) -> Vec<usize>; // likewise here
+
+    // fn outgoing_ledges(&self, label: L) -> Vec<L>; // should probably be changed to return an iterator instead
+
+    // fn incoming_ledges(&self, label: L) -> Vec<L>; // likewise here
+
+    fn delete_outgoing_edges(&self, vertex: usize); // deletes all outgoing edges of vertex; should return a Result
+
+    fn delete_incoming_edges(&self, vertex: usize); // deletes all incoming edges of vertex; should return a Result
 }
 
 // this trait applies to weighted graph structures
