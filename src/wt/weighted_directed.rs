3e318807aeff66feaf1b23db8e5ea8aa1f5039b6
MartinPerezLorenzo <martin.p-l@filesteak.com>
2024-07-01 16:15:37 +0200

    implemented methods, missing commit_edits

--- src/wt/weighted_directed.rs
+++ src/wt/weighted_directed.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use vers_vecs::RsVec;
 
 use crate::graph::weighted_directed::WeightedDigraph;
-use crate::traits::{Directed, Graph, UnLabeled, WTDirected, WTWeighted, Weighted, WT};
+use crate::traits::{Directed, Graph, UnLabeled, Unweighted, WTDirected, WTWeighted, Weighted, WT};
 use crate::wt::directed::WTDigraph;
 use crate::Edit;
 
@@ -13,88 +13,135 @@ pub struct WeightedWTDigraph<W> {
     weights: HashMap<(usize, usize), W>,
 }
 
-impl<W> WeightedWTDigraph<W>{
-    pub fn from_weighted_digraph(wdg : WeightedDigraph<W>) -> Self {
-        return WeightedWTDigraph{
+impl<W> WeightedWTDigraph<W> {
+    pub fn from_weighted_digraph(wdg: WeightedDigraph<W>) -> Self {
+        return WeightedWTDigraph {
             dg: WTDigraph::from_digraph(wdg.dg),
             weights_uncommitted: HashMap::new(),
             weights: wdg.weights,
-        }
+        };
     }
-    pub fn from(sequence: Vec<usize>, starting_indices: RsVec, weights : HashMap<(usize,usize), W>) -> Self {
-        return WeightedWTDigraph{
+    pub fn from(
+        sequence: Vec<usize>,
+        starting_indices: RsVec,
+        weights: HashMap<(usize, usize), W>,
+    ) -> Self {
+        return WeightedWTDigraph {
             dg: WTDigraph::from(sequence, starting_indices),
             weights_uncommitted: HashMap::new(),
             weights,
-        }
+        };
     }
 }
-impl<W> Graph<usize> for WeightedWTDigraph<W> {
+impl<W> Graph<usize> for WeightedWTDigraph<W> 
+where W: Clone,
+{
     fn add_vertex(&mut self, vertex: usize) -> usize {
-        todo!()
+        self.dg.add_vertex(vertex)
     }
 
     fn e_count(&self) -> usize {
-        todo!()
+        self.dg.e_count()
     }
 
     fn v_count(&self) -> usize {
-        todo!()
+        self.dg.v_count()
     }
 
     fn delete_edge(&mut self, from: usize, to: usize) {
-        todo!()
+        self.dg.delete_edge(from, to);
+        let old_weight = self.get_weight_updated(from, to); // checkme
+        self.weights_uncommitted
+            .insert((from, to), Edit::Delete(old_weight));
     }
 
     fn delete_vertex(&mut self, vertex: usize) {
-        todo!()
+        self.dg.delete_vertex(vertex);
     }
 
     fn vertex_exists(&self, vertex: usize) -> bool {
-        todo!()
+        self.dg.vertex_exists(vertex)
     }
 
     fn edge_exists(&self, from: usize, to: usize) -> bool {
-        todo!()
+        self.dg.edge_exists(from, to)
     }
 }
-impl<W> Directed<usize> for WeightedWTDigraph<W> {
+impl<W> Directed<usize> for WeightedWTDigraph<W> 
+where W : Clone,
+{
     fn outgoing_edges(&self, vertex: usize) -> Vec<usize> {
-        todo!()
+        self.dg.outgoing_edges(vertex)
     }
 
     fn incoming_edges(&self, vertex: usize) -> Vec<usize> {
-        todo!()
+        self.dg.incoming_edges(vertex)
     }
 
     fn delete_outgoing_edges(&mut self, vertex: usize) {
-        todo!()
+        // checkme
+        if !self.vertex_exists(vertex) {
+            panic!("delete_outgoing_edges: Vertex {} doesn't exist.", vertex);
+        }
+
+        let outgoing: Vec<usize> = self.outgoing_edges_updated(vertex);
+
+        for to in outgoing {
+            self.delete_edge(vertex, to); // this function call updates e_count_updated
+        }
+        // need to call self.delete_edge to update weights
+        self.dg.has_uncommitted_edits = true;
     }
 
     fn delete_incoming_edges(&mut self, vertex: usize) {
-        todo!()
+        // checkme
+        if !self.vertex_exists(vertex) {
+            panic!("incoming_edges: Vertex {} doesn't exist.", vertex);
+        }
+
+        let incoming: Vec<usize> = self.incoming_edges_updated(vertex); // empty list if there are no incoming edges
+
+        for from in incoming {
+            self.delete_edge(from, vertex); // this function call updates e_count_updated
+        }
+        // need to call self.delete_edge to update weights
+        self.dg.has_uncommitted_edits = true;
     }
 }
 impl<W> UnLabeled<usize> for WeightedWTDigraph<W> {
     fn append_vertex(&mut self) -> usize {
-        todo!()
+        self.dg.append_vertex()
     }
-    
+
     fn shrink(&mut self) -> Vec<Option<usize>> {
-        todo!()
+        self.dg.shrink()
     }
 }
-impl<W> Weighted<usize, W> for WeightedWTDigraph<W> {
+impl<W> Weighted<usize, W> for WeightedWTDigraph<W>
+where
+    W: Clone,
+{
     fn add_edge(&mut self, from: usize, to: usize, weight: W) {
-        todo!()
+        self.dg.add_edge(from, to);
+        self.weights_uncommitted
+            .insert((from, to), Edit::Add(weight));
     }
 
     fn edit_weight(&mut self, from: usize, to: usize, weight: W) {
-        todo!()
+        if self.edge_exists_updated(from, to) {
+            self.weights_uncommitted
+                .insert((from, to), Edit::Add(weight));
+        }
     }
 
     fn get_weight(&mut self, from: usize, to: usize) -> W {
-        todo!()
+        if self.edge_exists(from, to) {
+            if self.weights.contains_key(&(from, to)) {
+                let weight = self.weights.get(&(from, to)).unwrap().clone();
+                return weight;
+            }
+        }
+        panic!("wdg get_weight : edge does not exist or weight is missing");
     }
 }
 impl<W> WT<usize> for WeightedWTDigraph<W> {
@@ -107,32 +154,57 @@ impl<W> WT<usize> for WeightedWTDigraph<W> {
     // }
 
     fn discard_edits(&mut self) {
-        todo!()
+        self.dg.discard_edits();
+        self.weights_uncommitted = HashMap::new();
     }
 
     fn vertex_exists_updated(&self, vertex: usize) -> bool {
-        todo!()
+        self.dg.vertex_exists_updated(vertex)
     }
 
     fn edge_exists_updated(&self, from: usize, to: usize) -> bool {
-        todo!()
+        self.dg.edge_exists_updated(from, to)
     }
 
     fn v_count_updated(&self) -> usize {
-        todo!()
+        self.dg.v_count_updated()
     }
 }
 impl<W> WTDirected<usize> for WeightedWTDigraph<W> {
     fn outgoing_edges_updated(&self, vertex: usize) -> Vec<usize> {
-        todo!()
+        self.dg.outgoing_edges_updated(vertex)
     }
 
     fn incoming_edges_updated(&self, vertex: usize) -> Vec<usize> {
-        todo!()
+        self.dg.incoming_edges_updated(vertex)
     }
 }
-impl<W> WTWeighted<usize, W> for WeightedWTDigraph<W> {
+impl<W> WTWeighted<usize, W> for WeightedWTDigraph<W> 
+where W : Clone,
+{
     fn get_weight_updated(&mut self, from: usize, to: usize) -> W {
-        todo!()
+        if !self.vertex_exists_updated(from) {
+            panic!("wdg get_weight_updated : vertex from does not exist");
+        }
+        if !self.vertex_exists_updated(to){
+            panic!("wdg get_weight_updated : vertex to does not exist");
+        }
+        if !self.edge_exists_updated(from, to){
+            panic!("wdg get_weight_updated : edge from {from} to {to} does not exist");
+        }
+        // check if weight was editted
+        if self.weights_uncommitted.contains_key(&(from,to)){
+            let test = self.weights_uncommitted.get(&(from,to)).unwrap();
+            match self.weights_uncommitted.get(&(from,to)).unwrap(){
+                Edit::Add(weight) =>{
+                    return weight.clone();
+                },
+                Edit::Delete(_) => {
+                    panic!("wdg get_weight_updated : edge_exists_updated=true but weight is deleted");
+                },
+            }
+        } else {
+            return self.get_weight(from, to);
+        }
     }
 }
